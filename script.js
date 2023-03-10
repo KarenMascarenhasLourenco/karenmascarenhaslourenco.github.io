7b0df7a8e8502ad95a315fb7a49c29e4d66855a1
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-03-10 19:26:19 -0300

    att: new section on projects

---
+++ script.js
@@ -0,0 +1,34 @@
+const carousel = document.querySelector(".carousel");
+const container = document.querySelector(".carousel-container");
+const prevBtn = document.querySelector(".carousel-prev");
+const nextBtn = document.querySelector(".carousel-next");
+const slides = Array.from(document.querySelectorAll(".carousel .image-pack"));
+const spanSlide = Array.from(document.querySelectorAll('span'));
+
+let currentSlide = 0;
+
+function prevSlide() {
+  spanSlide[currentSlide].classList.toggle("span-active");
+  slides[currentSlide].classList.toggle("no-show");
+  currentSlide -= 1;
+  if (currentSlide >= slides.length) {
+    currentSlide = (slides.length - 1);
+  }
+  slides[currentSlide].classList.toggle("no-show");
+  spanSlide[currentSlide].classList.toggle("span-active");
+}
+
+function nextSlide() {
+  spanSlide[currentSlide].classList.toggle("span-active");
+  slides[currentSlide].classList.toggle('no-show');
+  currentSlide += 1;
+  if (currentSlide >= slides.length){
+    currentSlide = 0;
+  }
+  slides[currentSlide].classList.toggle("no-show");
+  spanSlide[currentSlide].classList.toggle("span-active");
+}
+
+prevBtn.addEventListener("click", prevSlide);
+nextBtn.addEventListener("click", nextSlide);
+setInterval(nextSlide,4000)
