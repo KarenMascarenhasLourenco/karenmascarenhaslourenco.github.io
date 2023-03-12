2706caa4557b6efdd4644147e742d7eed8b12971
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-03-11 22:29:41 -0300

    add: new files

--- script.js
+++ script.js
@@ -3,16 +3,26 @@ const container = document.querySelector(".carousel-container");
 const prevBtn = document.querySelector(".carousel-prev");
 const nextBtn = document.querySelector(".carousel-next");
 const slides = Array.from(document.querySelectorAll(".carousel .image-pack"));
-const spanSlide = Array.from(document.querySelectorAll('.span-carousel'));
+const spanSlide = Array.from(document.querySelectorAll("span"));
+const cardComponent = document.querySelectorAll(".card");
 
 let currentSlide = 0;
+
+function flipCard() {
+  this.classList.toggle("flip");
+}
+
+cardComponent.forEach(function (card) {
+  card.addEventListener("mouseover", flipCard);
+  card.addEventListener("mouseout", flipCard);
+});
 
 function prevSlide() {
   spanSlide[currentSlide].classList.toggle("span-active");
   slides[currentSlide].classList.toggle("no-show");
   currentSlide -= 1;
-  if (currentSlide >= slides.length) {
-    currentSlide = (slides.length - 1);
+  if (currentSlide <= 0) {
+    currentSlide = slides.length - 1;
   }
   slides[currentSlide].classList.toggle("no-show");
   spanSlide[currentSlide].classList.toggle("span-active");
@@ -20,9 +30,9 @@ function prevSlide() {
 
 function nextSlide() {
   spanSlide[currentSlide].classList.toggle("span-active");
-  slides[currentSlide].classList.toggle('no-show');
+  slides[currentSlide].classList.toggle("no-show");
   currentSlide += 1;
-  if (currentSlide >= slides.length){
+  if (currentSlide >= slides.length) {
     currentSlide = 0;
   }
   slides[currentSlide].classList.toggle("no-show");
