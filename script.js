013baabe8d8dec81f31b2a60f8be119d8b605056
PallasUlisses <karenmascarenhaslourenco@gmail.com>
2023-03-15 00:37:55 -0300

    att:all design portfolio

--- script.js
+++ script.js
@@ -1,44 +1,42 @@
-const carousel = document.querySelector(".carousel");
-const container = document.querySelector(".carousel-container");
-const prevBtn = document.querySelector(".carousel-prev");
-const nextBtn = document.querySelector(".carousel-next");
-const slides = Array.from(document.querySelectorAll(".carousel .image-pack"));
-const spanSlide = Array.from(document.querySelectorAll("span"));
-const cardComponent = document.querySelectorAll(".card");
+const prevBtn = document.querySelector(".carousel-prev"),
+ nextBtn = document.querySelector(".carousel-next"),
+ slides = Array.from(document.querySelectorAll(".carousel .image-pack")),
+ spanSlide = Array.from(document.querySelectorAll("span")),
+ cardComponent = document.querySelectorAll(".card");
 
 let currentSlide = 0;
 
 function flipCard() {
-  this.classList.toggle("flip");
+ this.classList.toggle("flip");
 }
 
 cardComponent.forEach(function (card) {
-  card.addEventListener("mouseover", flipCard);
-  card.addEventListener("mouseout", flipCard);
+ card.addEventListener("mouseover", flipCard);
+ card.addEventListener("mouseout", flipCard);
 });
 
 function prevSlide() {
-  spanSlide[currentSlide].classList.toggle("span-active");
-  slides[currentSlide].classList.toggle("no-show");
-  currentSlide -= 1;
-  if (currentSlide <= 0) {
-    currentSlide = slides.length - 1;
-  }
-  slides[currentSlide].classList.toggle("no-show");
-  spanSlide[currentSlide].classList.toggle("span-active");
+ spanSlide[currentSlide].classList.toggle("span-active");
+ slides[currentSlide].classList.toggle("no-show");
+ currentSlide -= 1;
+ if (currentSlide <= 0) {
+  currentSlide = slides.length - 1;
+ }
+ slides[currentSlide].classList.toggle("no-show");
+ spanSlide[currentSlide].classList.toggle("span-active");
 }
 
 function nextSlide() {
-  spanSlide[currentSlide].classList.toggle("span-active");
-  slides[currentSlide].classList.toggle("no-show");
-  currentSlide += 1;
-  if (currentSlide >= slides.length) {
-    currentSlide = 0;
-  }
-  slides[currentSlide].classList.toggle("no-show");
-  spanSlide[currentSlide].classList.toggle("span-active");
+ spanSlide[currentSlide].classList.toggle("span-active");
+ slides[currentSlide].classList.toggle("no-show");
+ currentSlide += 1;
+ if (currentSlide >= slides.length) {
+  currentSlide = 0;
+ }
+ slides[currentSlide].classList.toggle("no-show");
+ spanSlide[currentSlide].classList.toggle("span-active");
 }
 
 prevBtn.addEventListener("click", prevSlide);
 nextBtn.addEventListener("click", nextSlide);
-setInterval(nextSlide,4000)
+setInterval(nextSlide, 4000);
